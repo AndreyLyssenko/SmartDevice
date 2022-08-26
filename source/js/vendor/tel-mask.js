077fb7c119f2e40527904aa81eb41b6d9e6050c9
Andrey Lyssenko <andreylyssenko95@gmail.com>
2022-08-26 13:45:22 +0600

    Правки по баглисту

--- source/js/vendor/tel-mask.js
+++ source/js/vendor/tel-mask.js
@@ -1,21 +1,28 @@
 // https://webkaa.ru/javascript/maska-nomera-telefona-js
 
 const eventCallback = function (e) {
-    let el = e.target,
-    clearVal = el.dataset.phoneClear,
-    pattern = el.dataset.phonePattern,
-    matrixDef = "+7(___) ___-__-__",
-    matrix = pattern ? pattern : matrixDef,
-    i = 0,
-    def = matrix.replace(/\D/g, ""),
+  const formErrorMessage = e.target.nextElementSibling;
+  let el = e.target,
+  clearVal = el.dataset.phoneClear,
+  pattern = el.dataset.phonePattern,
+  matrixDef = "+7(___) ___-__-__",
+  matrix = pattern ? pattern : matrixDef,
+  i = 0,
+  def = matrix.replace(/\D/g, ""),
     val = e.target.value.replace(/\D/g, "");
     if (clearVal !== 'false' && e.type === 'blur') {
         if (val.length < matrix.match(/([\_\d])/g).length) {
             e.target.value = '';
+            formErrorMessage.classList.toggle('form-error-message--show');
+            e.target.style.border = "1px solid red";
             return;
         }
     }
-    if (def.length >= val.length) val = def;
+    if (def.length >= val.length) {
+      val = def;
+      e.target.style.border = 'none';
+      formErrorMessage.classList.remove('form-error-message--show');
+    }
     e.target.value = matrix.replace(/./g, function (a) {
         return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
     });
